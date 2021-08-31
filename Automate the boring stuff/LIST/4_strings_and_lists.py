62d95e63531c7ee6e51fa20eef13a45616398db4
bruno-gs <bgottsfritz@gmail.com>
2021-08-31 13:31:50 -0300

    finalizando listas

--- Automate the boring stuff/LIST/4_strings_and_lists.py
+++ Automate the boring stuff/LIST/4_strings_and_lists.py
@@ -1,3 +1,13 @@
+import copy
+spam = [1,2,3,4,5,6]
+
+numbers = copy.deepcopy(spam)
+numbers[1]='cheese'
+
+#faz um cópia da lista, criando outra referência
+#assim, pode alterar o numbers, sem mexer no spam 
+
+####################################################################################################
 # RECAP
 # Strings can do a lot of the same things lists can do, but strings are immutable.
 # Immutable values like strings and tuples cannot be modified "in place".
