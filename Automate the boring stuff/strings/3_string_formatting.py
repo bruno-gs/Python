26b536c4860511501f73b1a3268254d7eb7260d2
bruno-gs <bgottsfritz@gmail.com>
2021-09-10 13:13:58 -0300

    section9

---
+++ Automate the boring stuff/strings/3_string_formatting.py
@@ -0,0 +1,18 @@
+'''
+concatenar strings -- + 
+
+'hello' + 'world'
+
+name    = 'Alice'
+place   = 'main street'
+
+'hello' + name + 'you are invited to a party at' + place
+'''
+
+'''
+jeito mais facil
+
+'hello %s you are invited to a party at %s' % (name, place)
+'''
+
+
